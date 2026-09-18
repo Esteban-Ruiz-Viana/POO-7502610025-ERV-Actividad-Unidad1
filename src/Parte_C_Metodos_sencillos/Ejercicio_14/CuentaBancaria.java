38c9d2b4e1cbb50f07cd29a7ef697d90518ada6c
Esteban Ruiz Viana <eruiz@unicartagena.edu.co>
2026-09-18 10:39:59 -0500

    feat(Ejercicio_14): retirar saldo con validacion

---
+++ src/Parte_C_Metodos_sencillos/Ejercicio_14/CuentaBancaria.java
@@ -0,0 +1,41 @@
+package Parte_C_Metodos_sencillos.Ejercicio_14;
+
+
+// Ejercicio 14: retira solo si hay saldo suficiente
+
+
+public class CuentaBancaria {
+
+    //Atributos
+
+    String numero;
+    String titular;
+    double saldo;
+
+    //Metodo
+
+    void mostrarCuenta() {
+
+        System.out.println( "Cuenta N°: " + numero );
+        System.out.println( "Titular: " + titular );
+        System.out.println( "Saldo: $" + saldo );
+
+    }
+
+    void consignar(double valor) {
+        saldo = saldo + valor;
+
+   }
+
+   void retirar(double valor) {
+
+        if (valor  <= saldo) {
+            saldo = saldo - valor;
+
+            } else {
+
+            System.out.println("Saldo insuficiente para retirar $" + valor);
+
+        }
+   }
+}

6dc4431a9646aa69b24c36fd081f1bc29a83199a
Esteban Ruiz Viana <eruiz@unicartagena.edu.co>
2026-09-18 08:17:12 -0500

    feat(ejercicio01): crear clase Estudiante

---
+++ src/Parte_A_Reconocimiento_Clases/Estudiante_1/Estudiante.java
@@ -0,0 +1,25 @@
+package Parte_A_Reconocimiento_Clases.Estudiante_1;
+
+
+//Ejercicio 1: Definir una clase Estudiante con atributos nombre, codigo, semestre
+//y un metodo mostrarInfo().
+
+
+public class Estudiante {
+
+    //Atributos
+
+    String nombre;
+    int codigo;
+    int semestre;
+
+    //Metodo
+
+    void mostrarInfo() {
+
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Codigo: "+codigo);
+        System.out.println("Semestre: "+semestre);
+
+        }
+}

30f27e8320bd54410f5cd4d0dbe1a5c6220543ce
Esteban Ruiz Viana <eruiz@unicartagena.edu.co>
2026-09-18 14:17:38 -0500

    feat(Ejercicio_27): crear constructor para CuentaBancaria

---
+++ src/Parte_E_Constructores_commit/Ejercicio_27/CuentaBancaria.java
@@ -0,0 +1,54 @@
+package Parte_E_Constructores_commit.Ejercicio_27;
+
+
+// Ejercicio 27: Crear un constructor para CuentaBancaria
+// que reciba el número, titular y saldo inicial de la cuenta.
+
+
+public class CuentaBancaria {
+
+    //Atributos
+
+    String numero;
+    String titular;
+    double saldo;
+
+    //Constructor con parametros
+
+
+    //Metodo
+
+    CuentaBancaria(String numero, String titular, double saldo) {
+
+        this.numero = numero;
+        this.titular = titular;
+        this.saldo = saldo;
+
+    }
+
+
+    void mostrarCuenta() {
+
+        System.out.println( "Cuenta N°: " + numero );
+        System.out.println( "Titular: " + titular );
+        System.out.println( "Saldo: $" + saldo );
+
+    }
+
+    void consignar(double valor) {
+        saldo = saldo + valor;
+
+   }
+
+   void retirar(double valor) {
+
+        if (valor  <= saldo) {
+            saldo = saldo - valor;
+
+            } else {
+
+            System.out.println( "Saldo insuficiente para retirar $" + valor );
+
+        }
+   }
+}

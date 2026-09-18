a02ab39458bb5cd8baaaae07adbb3a9a485c9329
Esteban Ruiz Viana <eruiz@unicartagena.edu.co>
2026-09-18 08:44:36 -0500

    feat(Ejercicio_2): crear clase Vehiculo

---
+++ src/Parte_A_Reconocimiento_Clases/Estudiante_1/Ejercicio_2/Vehiculo.java
@@ -0,0 +1,25 @@
+package Parte_A_Reconocimiento_Clases.Estudiante_1.Ejercicio_2;
+
+
+// Ejercicio 2: Define una clase Vehiculo con atributos marca, modelo, velocidadActual
+// y un metodo mostrarEstado().
+
+
+public class Vehiculo {
+
+    //Atributos
+
+    String marca;
+    String modelo;
+    int velocidadActual;
+
+    //Metodo
+
+    void mostrarEstado() {
+
+        System.out.println( "Marca: " + marca);
+        System.out.println( "Modelo: "+ modelo);
+        System.out.println( "Velocidad: "+ velocidadActual " km/h");
+
+        }
+}

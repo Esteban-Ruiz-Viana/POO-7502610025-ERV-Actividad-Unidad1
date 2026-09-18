ec27ffdac94d632c40f7a6d5f1c2d05b1ea295b9
Esteban Ruiz Viana <eruiz@unicartagena.edu.co>
2026-09-18 15:39:38 -0500

    feat(Ejercicio_39): crear clase Cancion con titulo, artista, duracion y mostrarInfo

---
+++ src/Parte_G_Modelado_basico_de_situaciones/Ejercicio_39/Cancion.java
@@ -0,0 +1,19 @@
+package Parte_G_Modelado_basico_de_situaciones.Ejercicio_39;
+
+
+//
+
+public class Cancion {
+
+    String titulo;
+    String artista;
+    int duracion;
+
+    void mostrarInfo() {
+
+        System.out.println( "Título: " + titulo );
+        System.out.println( "Artista: " + artista );
+        System.out.println( "Duración: " + duracion + " segundos" );
+
+    }
+}

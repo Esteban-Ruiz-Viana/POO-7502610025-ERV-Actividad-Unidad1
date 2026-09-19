303170277bfa35345536c12098c1e7d0c5a65c6e
Esteban Ruiz Viana <eruiz@unicartagena.edu.co>
2026-09-18 23:12:24 -0500

    feat(Ejercicio_71): reescribir Estudiante con constructor con parametros

---
+++ src/Parte_L_Ejercicios_de_consolidacion_final/Ejercicio_71/Estudiante.java
@@ -0,0 +1,34 @@
+package Parte_L_Ejercicios_de_consolidacion_final.Ejercicio_71;
+
+// Ejercicio 71: Reescribe uno de tus primeros ejercicios, pero
+// ahora usando constructor con parámetros.
+
+public class Estudiante {
+
+    //Atributos
+
+    String nombre;
+    int codigo;
+    int semestre;
+
+    //Constructor con parametros
+
+    Estudiante(String nombre, int codigo, int semestre) {
+
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.semestre = semestre;
+
+    }
+
+
+    //Metodo
+
+    void mostrarInfo() {
+
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Codigo: "+codigo);
+        System.out.println("Semestre: "+semestre);
+
+        }
+}

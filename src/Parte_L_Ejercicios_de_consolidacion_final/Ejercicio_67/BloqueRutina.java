74620158ac86f720afbfd8c817dd9152e9adf63e
Esteban Ruiz Viana <eruiz@unicartagena.edu.co>
2026-09-18 22:27:21 -0500

    feat(Ejercicio_67): crear BloqueRutina y mostrar horario

---
+++ src/Parte_L_Ejercicios_de_consolidacion_final/Ejercicio_67/BloqueRutina.java
@@ -0,0 +1,20 @@
+package Parte_L_Ejercicios_de_consolidacion_final.Ejercicio_67;
+
+
+// Ejercicio 67: clase libre relacionada con mi vida cotidiana.
+// Basada en mi rutina real semanal y sus bloques de horario.
+
+
+public class BloqueRutina {
+
+        String dia;
+        String horaInicio;
+        String horaFin;
+        String actividad;
+
+        void mostrarBloque() {
+
+            System.out.println( dia + " " + horaInicio + " - " + horaFin + ": " + actividad );
+
+        }
+}
